c25e36187e97507c38d18ef7662eef9cc711b7eb
Ben <ben@amentum.space>
2020-08-04 11:57:40 +1000

    Add setup.py script and requirements.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="file_unittest",
+    version="0.0.1",
+    author="Amentum",
+    author_email="team@amentum.space",
+    description="A file-based unit test framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amentumspace/file_unittest.git",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
